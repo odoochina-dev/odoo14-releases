04c8e9bcc6eb1607968cebd378bc953ab54f7f64
odoochina-dev <znys@odoochina.dev>
2020-12-26 18:04:34 +0800

    odoo_14.0.20201226.tar.gz

--- odoo14_latest/odoo/release.py
+++ odoo14_latest/odoo/release.py
@@ -35,5 +35,5 @@ author_email = 'info@odoo.com'
 license = 'LGPL-3'
 
 nt_service_name = "odoo-server-" + series.replace('~','-')
-version += '-20201225' 
+version += '-20201226' 
 # hash = 'eb1d4cb3c66'
