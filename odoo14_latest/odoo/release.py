dfddcddc6ec4d3b3aabee906f321451fd68157a5
odoo-china-dev <znys@odoochina.dev>
2021-05-05 17:08:10 +0800

    odoo_14.0.20210122.tar.gz

--- odoo14_latest/odoo/release.py
+++ odoo14_latest/odoo/release.py
@@ -35,5 +35,5 @@ author_email = 'info@odoo.com'
 license = 'LGPL-3'
 
 nt_service_name = "odoo-server-" + series.replace('~','-')
-version += '-20210121' 
-# hash = '785fdb6cea6'
+version += '-20210122' 
+# hash = 'fd46b3dcfd0'
